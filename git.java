fb09ccd69763e22083fdbe151b8c99dbd164286a
小张同学 <zss123452022@163.com>
2022-09-26 10:56:19 +0800

    第五次提交

--- git.java
+++ git.java
@@ -2,6 +2,6 @@ public class git {
     public static  void main(String[] args){
         System.out.println("hello git");
         System.out.println("hello git222");
-        System.out.println("hello gi3333had哈萨克打开了");
+        System.out.println("hot-fix");
     }
 }
